3b000588aa6f67d4d63fd6a806c02257412a05df
BlackYps <jonas-fb@gmx.de>
2022-02-21 17:58:15 +0100

    Improve code style

--- icon_generator.py
+++ icon_generator.py
@@ -3,13 +3,14 @@
 from gimpfu import *
 import os
 
+
 def show_preview(loadfolder):
     image = pdb.gimp_image_new(80, 80, 0)
     iconlist = os.listdir(loadfolder)
     iconlist.sort()
     color = pdb.gimp_context_get_background()
     pdb.gimp_context_set_background((0.8, 0.8, 0.8, 1.0))
-    
+
     for icon in iconlist:
         if not icon.startswith("icon_"):
             continue
@@ -21,7 +22,7 @@ def show_preview(loadfolder):
         pdb.gimp_selection_invert(image)
         pdb.gimp_edit_bucket_fill(layer, 1, 28, 100, 0, 0, 0, 0)
     pdb.gimp_selection_none(image)
-        
+
     num_layers = len(image.layers)
     icon_w = 30
     icon_h = 28
@@ -32,14 +33,14 @@ def show_preview(loadfolder):
     w = cols * icon_w
     h = rows * icon_h
     image.resize(w, h, 5, 5)
-    
+
     idx = 0
-    for layer in reversed(image.layers): # Layers are in reverse order
+    for layer in reversed(image.layers):  # Layers are in reverse order
         x_trans = idx % cols * icon_w
         y_trans = idx / cols * icon_h
         layer.translate(x_trans, y_trans)
         idx += 1
-    
+
     pdb.gimp_context_set_background((0.7, 0.7, 0.9, 1.0))
     image.flatten()
     pdb.gimp_context_set_background(color)
@@ -60,9 +61,9 @@ def autoresize(image, layer, icondistance, tech):
     if new_h % 4 == 1 or new_h % 4 == 2:
         offset_y += 1
     if new_w % 4 > 0:
-        new_w +=  4 - (new_w % 4)
+        new_w += 4 - (new_w % 4)
     if new_h % 4 > 0:
-        new_h +=  4 - (new_h % 4)
+        new_h += 4 - (new_h % 4)
     if tech > 0:
         new_h += 4
         offset_y += 4
@@ -70,10 +71,14 @@ def autoresize(image, layer, icondistance, tech):
 
 
 def plugin_main(loadfolder, imagefolder, preview):
-    shapes = ['bomber', 'bot', 'commander', 'experimental', 'factory', 'factoryhq', 'fighter', 'gunship', 'land', 'ship', 'structure', 'sub', 'subcommander']
-    symbols = ['air', 'antiair', 'antiartillery', 'antimissile', 'antinavy', 'antishield', 'armored', 'artillery', 'bomb', 'counterintel', 'directfire', 'energy', 'energy_storage', 'engineer', 'generic', 'intel', 'land', 'mass', 'missile', 'naval', 'shield', 'sniper', 'transport', 'wall']
+    shapes = ['bomber', 'bot', 'commander', 'experimental', 'factory', 'factoryhq', 'fighter', 'gunship', 'land',
+              'ship', 'structure', 'sub', 'subcommander']
+    symbols = ['air', 'antiair', 'antiartillery', 'antimissile', 'antinavy', 'antishield', 'armored', 'artillery',
+               'bomb', 'counterintel', 'directfire', 'energy', 'energy_storage', 'engineer', 'generic', 'intel', 'land',
+               'mass', 'missile', 'naval', 'shield', 'sniper', 'transport', 'wall']
     types = ["over", "rest", "selected", "selectedover"]
-    image = pdb.file_png_load(os.path.join(imagefolder, "techmarkers.png"), os.path.join(imagefolder, "techmarkers.png"))
+    image = pdb.file_png_load(os.path.join(imagefolder, "techmarkers.png"),
+                              os.path.join(imagefolder, "techmarkers.png"))
     shapelayer = pdb.gimp_file_load_layer(image, os.path.join(imagefolder, "shapes.png"))
     pdb.gimp_image_insert_layer(image, shapelayer, None, -1)
     symbollayer = pdb.gimp_file_load_layer(image, os.path.join(imagefolder, "symbols.png"))
@@ -84,7 +89,7 @@ def plugin_main(loadfolder, imagefolder, preview):
     skipped_files = 0
     icondistance = 40
     pdb.gimp_image_resize(image, icondistance, icondistance, 0, 0)
-    
+
     for icon in iconlist:
         # Parse filenames
         if not icon.startswith("icon_"):
@@ -123,19 +128,19 @@ def plugin_main(loadfolder, imagefolder, preview):
             if type == "selected" or type == "selectedover":
                 offset += 1
             try:
-                pdb.gimp_layer_translate(save_img.layers[2], 0, tech * distance + offset) # Techlayer
-                if symbol == "wall": # Handle inconsistent naming of walls
-                    pdb.gimp_layer_translate(save_img.layers[1], types.index(type) * distance, 13 * distance) # Shapelayer
-                    pdb.gimp_layer_translate(save_img.layers[0], 0, symbols.index("generic") * distance) # Symbollayer
+                pdb.gimp_layer_translate(save_img.layers[2], 0, tech * distance + offset)  # Techlayer
+                if symbol == "wall":  # Handle inconsistent naming of walls
+                    pdb.gimp_layer_translate(save_img.layers[1], types.index(type) * distance, 13 * distance)  # Shapelayer
+                    pdb.gimp_layer_translate(save_img.layers[0], 0, symbols.index("generic") * distance)  # Symbollayer
                 else:
-                    pdb.gimp_layer_translate(save_img.layers[1], types.index(type) * distance, shapes.index(shape) * distance) # Shapelayer
-                    pdb.gimp_layer_translate(save_img.layers[0], 0, symbols.index(symbol) * distance) # Symbollayer
+                    pdb.gimp_layer_translate(save_img.layers[1], types.index(type) * distance, shapes.index(shape) * distance)  # Shapelayer
+                    pdb.gimp_layer_translate(save_img.layers[0], 0, symbols.index(symbol) * distance)  # Symbollayer
             except ValueError:
                 errors += "ValueError: Cannot parse file " + icon + "\n"
             else:
                 if type == "over" or type == "selectedover":
                     pdb.gimp_drawable_invert(save_img.layers[0], 0)
-                    
+
                 layer = pdb.gimp_image_merge_visible_layers(save_img, CLIP_TO_IMAGE)
                 autoresize(save_img, layer, icondistance, tech)
                 layer = pdb.gimp_image_merge_visible_layers(save_img, CLIP_TO_IMAGE)
@@ -147,19 +152,21 @@ def plugin_main(loadfolder, imagefolder, preview):
                 pdb.file_dds_save(save_img, layer, outputname, outputname, 1, 0, 0, 0, -1, 0, 0, 0, 0, 2.2, 0, 0, 0.5)
                 pdb.gimp_image_delete(save_img)
     if len(errors) > 0:
-        pdb.gimp_message("The following errors occured:\n" + errors + str(skipped_files) + " additional files/folders were skipped")
+        pdb.gimp_message(
+            "The following errors occured:\n" + errors + str(skipped_files) + " additional files/folders were skipped")
     elif skipped_files > 0:
         pdb.gimp_message(str(skipped_files) + " files/folders were skipped")
     if preview:
         pdb.gimp_progress_set_text("generating preview file")
         show_preview(outputfolder)
 
+
 register(
     "python_fu_icon_generator",
     "generate new icons for FAF",
     "generates new icons for FAF out of lookup files",
-    "BlackYps", 
-    "BlackYps", 
+    "BlackYps",
+    "BlackYps",
     "2020",
     "<Toolbox>/File/Create/Icon Generation",
     "",
