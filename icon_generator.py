5cc119cf0e0e3fc791d3dac95e75ce446be2cd10
BlackYps <jonas-fb@gmx.de>
2023-04-30 14:58:28 +0200

    Fix alignment of selected icons

--- icon_generator.py
+++ icon_generator.py
@@ -66,9 +66,7 @@ def autoresize(image, layer, icondistance, tech, shape, selected):
         new_h += 4 - (new_h % 4)
 
     # This will likely need to be changed when the shape sizes change
-    if selected and tech > 1:
-        offset_y -= 1
-    elif not selected and tech > 1 and shape in ["fighter", "ship", "sub"]:
+    if not selected and tech > 1 and shape in ["fighter", "ship", "sub"]:
         offset_y += 1
     # Center the images with tech markers
     if tech > 1:
